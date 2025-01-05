527a24582f3a4e781da18178bcc3ce3da21817e3
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-05 21:39:10 +0100

    add version 2 of find_digits_product function

--- thuat_toan_co_ban/so_hoc/digits_product.py
+++ thuat_toan_co_ban/so_hoc/digits_product.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-01-04 23:33:27
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-01-05 21:34:05
+# @Last Modified time: 2025-01-05 21:38:10
 
 """
 Cho trước số tự nhiên number
@@ -52,14 +52,42 @@ def find_digits_product(number: int) -> int:
     return reverse_digits(digits_product) if number == 1 else -1
 
 
+def find_digits_product_v2(number: int) -> int:
+    """
+    Returns the smallest positive number such that
+    the product of its digits equals the given number.
+    """
+    # edge cases
+    if number == 0:
+        return 10
+    if number == 1:
+        return 1
+
+    # regular case
+    digits_product = ""
+    for i in range(9, 1, -1):
+        while number % i == 0:
+            # add i to the first
+            digits_product = str(i) + digits_product
+            number //= i
+    return int(digits_product) if number == 1 else -1
+
+
 def dry_tests():
     """
     Dry tests
     """
     test_cases = [(12, 26), (19, -1), (450, 2559), (0, 10), (13, -1), (108, 269)]
+    print("Tests on version 1:")
     for number, ground_truth in test_cases:
         result = find_digits_product(number)
-        print(f"find_digits_product({number}) = {result}: {result == ground_truth}")
+        print(f"- find_digits_product({number}) = {result}: {result == ground_truth}")
+    print("Tests on version 2:")
+    for number, ground_truth in test_cases:
+        result = find_digits_product_v2(number)
+        print(
+            f"- find_digits_product_v2({number}) = {result}: {result == ground_truth}"
+        )
 
 
 if __name__ == "__main__":
