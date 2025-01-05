8a2adca04651e45a1c3b86d9d94ee69ba44dbadd
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-05 21:34:15 +0100

    add algo explaination

--- thuat_toan_co_ban/so_hoc/digits_product.py
+++ thuat_toan_co_ban/so_hoc/digits_product.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-01-04 23:33:27
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-01-04 23:47:21
+# @Last Modified time: 2025-01-05 21:34:05
 
 """
 Cho trước số tự nhiên number
@@ -12,6 +12,15 @@ Nếu không có số thỏa mãn, trả ra -1.
 Ví dụ:
 - digitsProduct(12) = 26
 - digitsProduct(19) = -1
+- digitsProduct(108) = 269
+
+Giải thích:
+108 = 9 * 6 * 2
+    = 9 * 4 * 3
+    = 3 * 3 * 3 * 2 * 2
+=> kết quả: 269 vì:
+- (9, 6, 2) và (9, 4, 3) tối ưu về số chữ số.
+- (9, 6, 2) tối ưu hơn (9, 4, 3) vì lấy 2 tốt hơn 3 khi làm chữ số đầu tiên.
 """
 
 
@@ -27,14 +36,17 @@ def find_digits_product(number: int) -> int:
     Returns the smallest positive number such that
     the product of its digits equals the given number.
     """
+    # edge cases
     if number == 0:
         return 10
     if number == 1:
         return 1
 
+    # regular case
     digits_product = 0
     for i in range(9, 1, -1):
         while number % i == 0:
+            # add i to the last
             digits_product = digits_product * 10 + i
             number //= i
     return reverse_digits(digits_product) if number == 1 else -1
