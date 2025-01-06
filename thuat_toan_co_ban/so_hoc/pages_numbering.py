67d0ff2333b19f462d56530b29760df23e2e01e3
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-06 23:32:33 +0100

    solution of problem pagesNumbering

---
+++ thuat_toan_co_ban/so_hoc/pages_numbering.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-06 23:15:51
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-06 23:31:40
+
+"""
+Cho biết số trang sách của 1 quyển sách.
+Hãy tìm số lượng các chữ số dùng để đánh số trang của quyển sách đó.
+
+Ví dụ:
+- pagesNumbering(n) = 13.
+13 chữ số được sử dụng là: 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1
+"""
+
+
+def get_number_of_digits_used(no_pages: int) -> int:
+    """
+    Returns the number of digits used to number the pages of a book
+    """
+    no_digits_used = 0
+    degree = 1
+    current_digits_used = 10**degree - 10 ** (degree - 1)
+    while no_pages > current_digits_used:
+        no_pages -= current_digits_used
+        no_digits_used += current_digits_used * degree
+        degree += 1
+        current_digits_used = 10**degree - 10 ** (degree - 1)
+
+    if no_pages > 0:
+        no_digits_used += no_pages * degree
+    return no_digits_used
+
+
+def dry_tests():
+    """
+    Dry tests
+    """
+    test_cases = [(11, 13), (1000, 2893), (13, 17), (1, 1)]
+    for no_pages, ground_truth in test_cases:
+        result = get_number_of_digits_used(no_pages)
+        print(
+            f"get_number_of_digits_used({no_pages}) = {result}: {result == ground_truth}"
+        )
+
+
+if __name__ == "__main__":
+    dry_tests()
