83d1defb0bb2456c1a06fb7633352eb6c8a0cbb2
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-07 23:49:10 +0100

    Problem primeSum

---
+++ thuat_toan_co_ban/so_hoc/prime_sum.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-07 23:48:42
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-07 23:48:42
+
+"""
+Cho số nguyên n, hãy tính tổng các số nguyên tố nhỏ hơn hoặc bằng n.
+
+Bởi vì tổng các số nguyên tố nhỏ hơn hoặc bằng n có thể rất lớn,
+nên hãy trả ra kết quả của tổng trên dưới dạng là số dư trong phép chia của tổng trên cho 22082018.
+
+Ví dụ:
+- Với n = 2 thì primeSum(n) = 2
+- Với n = 3 thì primeSum(n) = 5
+"""
