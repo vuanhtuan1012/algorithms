600480ea6f24aaee83b4327c3b5074554b427940
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-10 23:21:02 +0100

    problem numberZeroDigits

---
+++ thuat_toan_co_ban/so_hoc/number_zero_digits.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-10 23:19:15
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-10 23:20:12
+
+"""
+Cho số tự nhiên n.
+Hãy tính số chữ số 0 tận cùng của n!
+
+Ví dụ:
+- numberZeroDigits(5) = 1 vì 5! = 120
+"""
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
