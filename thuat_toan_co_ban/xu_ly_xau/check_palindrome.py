75a7e2887534d1edf52d302d08de45e816b4c0c8
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-13 22:41:34 +0100

    solution of problem checkPalindrome

--- thuat_toan_co_ban/xu_ly_xau/check_palindrome.py
+++ thuat_toan_co_ban/xu_ly_xau/check_palindrome.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-01-13 22:31:39
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-01-13 22:33:44
+# @Last Modified time: 2025-01-13 22:40:54
 
 """
 Một xâu được gọi là palindrome nếu viết xuôi hay viết ngược xâu đó đều cho ra kết quả giống nhau
@@ -13,3 +13,31 @@ Ví dụ:
 - checkPalindrome("abac") = False
 - checkPalindrome("a") = True
 """
+
+
+def is_palindrome(string: str) -> bool:
+    """
+    Returns True if given string is palindrome, otherwise False
+    """
+    reversed_string = string[::-1]
+    return string == reversed_string
+
+
+def dry_tests():
+    """
+    Dry tests
+    """
+    test_cases = [
+        ("aabaa", True),
+        ("abac", False),
+        ("a", True),
+        ("az", False),
+        ("abba", True),
+    ]
+    for string, ground_truth in test_cases:
+        result = is_palindrome(string)
+        print(f"is_palindrome({string}) = {result}: {result == ground_truth}")
+
+
+if __name__ == "__main__":
+    dry_tests()
