2c8060eacee711ef2bb6d739b16c9e59432ebbc2
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-13 23:05:01 +0100

    problem isTandemRepeat

---
+++ thuat_toan_co_ban/xu_ly_xau/is_tandem_repeat.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-13 23:01:38
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-13 23:04:34
+
+"""
+Cho một xâu kí tự s
+Hãy kiểm tra xem xâu s có thể tạo ra bằng cách ghép một xâu vào chính nó hay không?
+
+Ví dụ:
+- isTandemRepeat("tandemtandem") = True vì tandem x 2 = tandemtandem
+- isTandemRepeat("qqq") = False vì q x 2 != qqq
+- isTandemRepeat("2w2ww") = False vì 2w x 2 != 2w2ww
+"""
