c36f5c836a95582792b990fb0efde6aebda7e31d
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-13 22:57:06 +0100

    solution of problem formatString

--- thuat_toan_co_ban/xu_ly_xau/format_string.py
+++ thuat_toan_co_ban/xu_ly_xau/format_string.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-01-13 22:44:57
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-01-13 22:45:37
+# @Last Modified time: 2025-01-13 22:56:34
 
 """
 Xóa các khoảng trắng thừa (kí tự cách) trong xâu kí tự cho trước,
@@ -13,3 +13,35 @@ Ví dụ:
 - formatString(" abc   a aa  a a ") = "abc a aa a a"
 
 """
+
+
+def format_string(string: str) -> str:
+    """
+    Returns string formated
+    """
+    string_formated = ""
+    for char in string:
+        if char != " ":
+            string_formated += char
+            continue
+        if string_formated and string_formated[-1] != " ":
+            string_formated += char
+    return string_formated.strip()
+
+
+def dry_tests():
+    """
+    Dry tests
+    """
+    test_cases = [
+        (" abc   a aa  a a ", "abc a aa a a"),
+        ("abcd", "abcd"),
+        ("        a           ", "a"),
+    ]
+    for string, ground_truth in test_cases:
+        result = format_string(string)
+        print(f"format_string({string}) = {result}: {result == ground_truth}")
+
+
+if __name__ == "__main__":
+    dry_tests()
