5f0d2d080ef63289f1d043498aeedc69672b34d0
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-13 22:51:58 +0100

    problem formatString

---
+++ thuat_toan_co_ban/xu_ly_xau/format_string.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-13 22:44:57
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-13 22:45:37
+
+"""
+Xóa các khoảng trắng thừa (kí tự cách) trong xâu kí tự cho trước,
+sao cho giữa các từ chỉ cách nhau bởi 1 khoảng trống.
+Cũng không có khoảng trống ở đầu và cuối của xâu.
+
+Ví dụ:
+- formatString(" abc   a aa  a a ") = "abc a aa a a"
+
+"""
