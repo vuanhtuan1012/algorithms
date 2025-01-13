0da71c7e6b670a1006ebdd10cac9a03cf5e9ca47
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-13 22:34:13 +0100

    problem checkPalindrome

---
+++ thuat_toan_co_ban/xu_ly_xau/check_palindrome.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-13 22:31:39
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-13 22:33:44
+
+"""
+Một xâu được gọi là palindrome nếu viết xuôi hay viết ngược xâu đó đều cho ra kết quả giống nhau
+Cho một xâu kí tự, kiểm tra nó có phải xâu palindrome không.
+
+Ví dụ:
+- checkPalindrome("aabaa") = True
+- checkPalindrome("abac") = False
+- checkPalindrome("a") = True
+"""
