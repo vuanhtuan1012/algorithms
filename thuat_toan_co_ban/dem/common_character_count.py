3ecd647abc01a45813f74528c9a56c968cb0a9fc
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-21 17:17:17 +0700

    problem commonCharacterCount

---
+++ thuat_toan_co_ban/dem/common_character_count.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-21 17:15:12
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-21 17:16:45
+
+"""
+Cho hai xâu kí tự, tìm số lượng kí tự chung giữa chúng.
+
+Ví dụ:
+- commonCharacterCount("aabcc", "adcaa") = 3 vì chúng có chung 2 kí tự "a" và 1 kí tự "c"
+"""
