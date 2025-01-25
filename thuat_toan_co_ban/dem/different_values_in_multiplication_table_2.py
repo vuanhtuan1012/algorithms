13b6f536b8227374fe8665e573d14fc9cf5f5d7b
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-25 23:41:54 +0700

    problem differentValuesInMultiplicationTable2

---
+++ thuat_toan_co_ban/dem/different_values_in_multiplication_table_2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-25 23:38:47
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-25 23:41:00
+"""
+Cho một bảng có n hàng và m cột.
+Mỗi một ô trong bảng ở hàng i và cột j chứa 1 số có giá trị
+bằng i * j (chỉ số hàng và cột đều được đếm bắt đầu từ 1)
+Cho trước n và m, hãy tìm số lượng các số nguyên khác nhau nằm trong bảng.
+
+Ví dụ:
+- differentValuesInMultiplicationTable2(3, 2) = 5
+  vì có 5 giá trị khác nhau trong bảng là: 1, 2, 3, 4, 6
+"""
