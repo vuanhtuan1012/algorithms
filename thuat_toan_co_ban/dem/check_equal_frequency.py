e1da43ef1075fca44937c23c495b4242ebf69e28
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-26 23:26:08 +0700

    problem checkEqualFrequency

---
+++ thuat_toan_co_ban/dem/check_equal_frequency.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-26 23:23:53
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-26 23:25:36
+
+"""
+Cho một mảng số nguyên
+Hãy kiểm tra xem tần số xuất hiện của tất cả các số nguyên trong mảng
+có bằng nhau hay không
+
+Ví dụ:
+- checkEqualFrequency([1, 2, 2, 1]) = true
+- checkEqualFrequency([1, 2, 2, 3, 1, 3, 1, 3]) = false
+"""
