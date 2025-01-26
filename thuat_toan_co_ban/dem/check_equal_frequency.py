76a128d7c275fcacca4402190dd5071368cad302
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-27 00:16:32 +0700

    add unit tests for problem checkEqualFrequency

--- thuat_toan_co_ban/dem/check_equal_frequency.py
+++ thuat_toan_co_ban/dem/check_equal_frequency.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-01-26 23:23:53
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-01-26 23:56:06
+# @Last Modified time: 2025-01-27 00:10:57
 
 """
 Cho một mảng số nguyên
@@ -14,6 +14,7 @@ Ví dụ:
 - checkEqualFrequency([1, 2, 2, 3, 1, 3, 1, 3]) = false
 """
 from typing import List, Dict
+
 
 def get_frequencies(array: List[int]) -> Dict[int, int]:
     """
@@ -32,7 +33,7 @@ def check_equal_frequency(array: List) -> bool:
     """
     frequencies = get_frequencies(array)
     standard = frequencies[array[0]]
-    return all(frequencies[element] == standard for element in frequencies)
+    return all(freq == standard for _, freq in frequencies.items())
 
 
 def dry_tests():
@@ -41,5 +42,17 @@ def dry_tests():
     """
     test_cases = [
         ([1, 2, 2, 1], True),
-        ([1, 2, 2, 3, 1, 3, 1, 3], False)
+        ([1, 2, 2, 3, 1, 3, 1, 3], False),
+        ([239], True),
+        ([239, 240, 241], True),
+        ([34, 23, 33, 23], False),
+        ([1, 1, 1, 1, 1], True),
     ]
+
+    for i, (array, ground_truth) in enumerate(test_cases, start=1):
+        result = check_equal_frequency(array)
+        print(f"Test case {i}: {result == ground_truth}")
+
+
+if __name__ == "__main__":
+    dry_tests()
