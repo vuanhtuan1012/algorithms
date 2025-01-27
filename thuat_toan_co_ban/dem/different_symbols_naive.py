bb31a9509fd32f623315addd4fad748f270171cc
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-28 06:30:25 +0700

    problem differentSymbolsNaive

---
+++ thuat_toan_co_ban/dem/different_symbols_naive.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-28 06:28:36
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-28 06:29:33
+
+"""
+Cho xâu kí tự. Hãy đếm số lượng kí tự khác nhau trong xâu đó
+
+Ví dụ: differentSymbolsNaive("cabca") = 3
+"""
