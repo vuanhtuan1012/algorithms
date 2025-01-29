fb5c10ba8c66f791beed7e15935ec1f0b800679a
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-29 23:49:48 +0700

    problem differentSubstringsTrie

---
+++ thuat_toan_co_ban/dem/different_substrings_trie.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-29 23:47:44
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-29 23:49:24
+
+"""
+Cho một xâu kí tự, tìm số lượng xâu con khác nhau của xâu đó (không tính xâu rỗng).
+
+Ví dụ:
+- differentSubstringsTrie("abac") = 9
+  9 xâu con khác nhau của xâu đầu vào là:
+  "a", "b", "c", "ab", "ac", "ba", "aba", "bac", "abac"
+"""
