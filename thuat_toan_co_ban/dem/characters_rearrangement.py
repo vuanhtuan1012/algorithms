f93996dc3d1f0c0fd3868b1044cacf615b999314
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-01-31 23:56:14 +0700

    problem charactersRearrangement

---
+++ thuat_toan_co_ban/dem/characters_rearrangement.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-01-31 23:54:22
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-01-31 23:55:50
+
+"""
+Cho hai xâu kí tự, hãy kiểm tra xem có tồn tại phương án
+đổi chỗ các kí tự ở trong xâu thứ nhất để trở thành xâu thứ hai được hay không?
+
+Ví dụ:
+- charactersRearrangement("abcd", "cbad") = True
+- charactersRearrangement("a", "b") = False
+"""
