36586dd649cb7efc0c8e7fe34203108e4e06b295
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-01 19:42:38 +0700

    solution of problem charactersRearrangement

--- thuat_toan_co_ban/dem/characters_rearrangement.py
+++ thuat_toan_co_ban/dem/characters_rearrangement.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-01-31 23:54:22
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-01-31 23:55:50
+# @Last Modified time: 2025-02-01 19:41:46
 
 """
 Cho hai xâu kí tự, hãy kiểm tra xem có tồn tại phương án
@@ -12,3 +12,43 @@ Ví dụ:
 - charactersRearrangement("abcd", "cbad") = True
 - charactersRearrangement("a", "b") = False
 """
+from typing import Dict
+
+
+def get_frequency(string: str) -> Dict[str, int]:
+    """
+    Returns frequency of letters in the given string
+    """
+    freq = {}
+    for letter in string:
+        freq[letter] = freq.get(letter, 0) + 1
+    return freq
+
+
+def is_permutable(first_string: str, second_string: str) -> bool:
+    """
+    Returns True if two strings are permutation, otherwise False
+    """
+    return get_frequency(first_string) == get_frequency(second_string)
+
+
+def dry_tests():
+    """
+    Dry tests
+    """
+    test_cases = [
+        ("aaad", "aaa", False),
+        ("abcd", "cbad", True),
+        ("a", "b", False),
+        ("aaa", "aaaa", False),
+        ("abcdef", "badcfe", True),
+    ]
+    for i, (first_string, second_string, ground_truth) in enumerate(
+        test_cases, start=1
+    ):
+        result = is_permutable(first_string, second_string)
+        print(f"Test case {i}: {result == ground_truth}")
+
+
+if __name__ == "__main__":
+    dry_tests()
