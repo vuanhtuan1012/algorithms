79573452b47040c0ab570dca3921c3723619e277
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-01 23:45:25 +0700

    problem isPangram

---
+++ thuat_toan_co_ban/dem/is_pangram.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-02-01 23:43:21
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-02-01 23:44:49
+
+"""
+Một pangram là một câu mà mỗi kí tự chữ cái (a-z) được sử dụng ít nhất một lần.
+Cho một xâu kí tự, kiểm tra xem nó có phải là một pangram hay không?
+
+Ví dụ:
+- isPangram("The quick brown fox jumps over the lazy dog.") = True
+- isPangram("abcdefghijklmnopqrstuvwxya") = False
+"""
