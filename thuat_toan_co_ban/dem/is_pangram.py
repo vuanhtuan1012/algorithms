1867944eb59682bbbf48bdd82f834f42c13cfa35
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-01 23:49:36 +0700

    solution of problem isPangram

--- thuat_toan_co_ban/dem/is_pangram.py
+++ thuat_toan_co_ban/dem/is_pangram.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-02-01 23:43:21
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-02-01 23:44:49
+# @Last Modified time: 2025-02-01 23:49:00
 
 """
 Một pangram là một câu mà mỗi kí tự chữ cái (a-z) được sử dụng ít nhất một lần.
@@ -12,3 +12,14 @@ Ví dụ:
 - isPangram("The quick brown fox jumps over the lazy dog.") = True
 - isPangram("abcdefghijklmnopqrstuvwxya") = False
 """
+
+
+def is_pangram(sentence: str) -> bool:
+    """
+    Returns True if the given sentence is pangram, otherwise False
+    """
+    for code in range(ord("a"), ord("z") + 1):
+        letter = chr(code)
+        if letter not in sentence:
+            return False
+    return True
