2fb764f3d7d582f4adc5de3d20bf5df8d674a1a5
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-02 18:50:13 +0700

    add solution of problem differentSquares

--- thuat_toan_co_ban/dem/different_squares.py
+++ thuat_toan_co_ban/dem/different_squares.py
@@ -2,11 +2,11 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-02-02 17:39:54
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-02-02 17:41:17
+# @Last Modified time: 2025-02-02 18:49:43
 
 """
 Cho một ma trận chữ nhật chứa các chữ số (0-9).
-Hãy tính số lượng các hình vuông 2 x 2 khác nhau tồn tại trong ma trận.
+Hãy tính số lượng các hình vuông 2x2 khác nhau tồn tại trong ma trận.
 
 Ví dụ: với matrix = [
     [1, 2, 1],
@@ -17,3 +17,26 @@ Ví dụ: với matrix = [
 ]
 thì kết quả differentSquares(matrix) = 6.
 """
+from typing import List
+
+
+def to_string(square: List[List[int]]) -> str:
+    """
+    Returns the string which represent the given square
+    """
+    return "\n".join(map(str, ("".join(map(str, row)) for row in square)))
+
+
+def count_different_squares(matrix: List[List[int]]) -> int:
+    """
+    Returns number of different 2x2 squares in the given matrix
+    """
+    squares = set()
+    no_rows = len(matrix)
+    no_columns = len(matrix[0]) if no_rows > 0 else 0
+    for i in range(no_rows - 1):
+        for j in range(no_columns - 1):
+            square = [matrix[i][j : j + 2], matrix[i + 1][j : j + 2]]
+            str_square = to_string(square)
+            squares.add(str_square)
+    return len(squares)
