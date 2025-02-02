22227f381907df1f1a4d62d9489da86f919344f9
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-02 17:41:45 +0700

    problem differentSquares

---
+++ thuat_toan_co_ban/dem/different_squares.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-02-02 17:39:54
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-02-02 17:41:17
+
+"""
+Cho một ma trận chữ nhật chứa các chữ số (0-9).
+Hãy tính số lượng các hình vuông 2 x 2 khác nhau tồn tại trong ma trận.
+
+Ví dụ: với matrix = [
+    [1, 2, 1],
+    [2, 2, 2],
+    [2, 2, 2],
+    [1, 2, 3],
+    [2, 2, 1]
+]
+thì kết quả differentSquares(matrix) = 6.
+"""
