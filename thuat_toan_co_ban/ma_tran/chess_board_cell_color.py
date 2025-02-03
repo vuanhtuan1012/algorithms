1b35a075a73dd3cbcd06feec3afe386dcb7e3f84
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-03 11:20:30 +0700

    problem chessBoardCellColor

---
+++ thuat_toan_co_ban/ma_tran/chess_board_cell_color.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-02-03 11:18:42
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-02-03 11:19:57
+
+"""
+Cho địa chỉ hai ô trên bàn cờ vua tiêu chuẩn, hãy kiểm tra chúng cùng màu hay khác màu.
+
+Ví dụ:
+- chessBoardCellColor("A1", "C3") = True
+- chessBoardCellColor("A1", "H3") = False
+"""
