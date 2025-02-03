3936524232a20af29e05d08b4b503e7306d91d26
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-03 11:48:00 +0700

    add unit tests to problem chessBoardCellColor

--- thuat_toan_co_ban/ma_tran/chess_board_cell_color.py
+++ thuat_toan_co_ban/ma_tran/chess_board_cell_color.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-02-03 11:18:42
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-02-03 11:38:12
+# @Last Modified time: 2025-02-03 11:47:31
 
 """
 Cho địa chỉ hai ô trên bàn cờ vua tiêu chuẩn, hãy kiểm tra chúng cùng màu hay khác màu.
@@ -30,3 +30,23 @@ def is_same_color(first_cell: str, second_cell: str) -> bool:
         get_indexes(first_cell)[i] - get_indexes(second_cell)[i] for i in range(2)
     )
     return diff % 2 == 0
+
+
+def dry_tests():
+    """
+    Dry tests
+    """
+    test_cases = [
+        ("A1", "C3", True),
+        ("A1", "H3", False),
+        ("A1", "A2", False),
+        ("A1", "B2", True),
+        ("B3", "H8", False),
+    ]
+    for i, (first_cell, second_cell, ground_truth) in enumerate(test_cases, start=1):
+        result = is_same_color(first_cell, second_cell)
+        print(f"Test case {i}: {result == ground_truth}")
+
+
+if __name__ == "__main__":
+    dry_tests()
