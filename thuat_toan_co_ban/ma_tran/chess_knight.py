ddd9d98a6f03dac6843be981d7d98cbed35f8048
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-05 17:43:23 +0700

    solution of problem chessKnight

--- thuat_toan_co_ban/ma_tran/chess_knight.py
+++ thuat_toan_co_ban/ma_tran/chess_knight.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-02-05 16:08:03
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-02-05 16:13:44
+# @Last Modified time: 2025-02-05 17:42:55
 
 """
 Bàn cờ vua là một bảng có 8*8 ô vuông.
@@ -18,3 +18,28 @@ Mỗi ô trên bàn cờ được kí hiệu bằng 2 kí tự - 1 kí tự ch�
 Cho biết vị trí của con mã trên bàn cờ vua.
 Hãy tìm số vị trí khác nhau mà con mã có thể thực hiện nước nhảy.
 """
+from typing import Tuple
+
+
+def square_to_coordinate(square: str) -> Tuple[int, int]:
+    """
+    Returns the coordinate of the given square
+    """
+    return ord(square[0].upper()), int(square[1])
+
+
+def get_no_positions(knight_square: str) -> int:
+    """
+    Returns the number of different positions that the given knight can jump to
+    """
+    # changes in coordinate when the knight move
+    changes = [(-1, -2), (-1, 2), (1, -2), (1, 2), (-2, -1), (-2, 1), (2, -1), (2, 1)]
+
+    no_positions = 0
+    current_coordinate = square_to_coordinate(knight_square)
+    for col_delta, row_delta in changes:
+        col = current_coordinate[0] + col_delta
+        row = current_coordinate[1] + row_delta
+        if ord("A") <= col <= ord("H") and 1 <= row <= 8:
+            no_positions += 1
+    return no_positions
