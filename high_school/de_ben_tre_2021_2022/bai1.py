689aa6b1c0c86d83e6469610389f08bea80835e0
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-07 06:06:02 +0700

    bai 1 de HSG tinh Ben Tre 2021-2022

---
+++ high_school/de_ben_tre_2021_2022/bai1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-02-07 05:59:15
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-02-07 06:05:22
+
+"""
+Chia mảng
+
+Cho dãy số nguyên không âm a1, a2..., an với 1 <= n <= 1000.
+Hãy lập trình tìm cách chia dãy số trên thành 2 đoạn a[1..i] và a[i+1..n]
+sao cho tổng các phần tử trong mỗi đoạn bằng nhau (1 <= i < n).
+
+Dữ liệu:
+- Dòng 1: chứa số nguyên dương n
+- Dòng 2: chứa n số nguyên không âm a1, a2..., an.
+
+Kết quả: ghi ra chỉ số i tìm được. Nếu không có kết quả thì ghi ra số 0
+
+Ví dụ:
+- File input: chiamang.inp
+5
+2 2 3 6 1
+- File output: chiamang.out
+3
+"""
