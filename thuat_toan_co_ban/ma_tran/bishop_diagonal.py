0c778751698db06b52a2d160c57e341f6f380666
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-09 23:58:58 +0700

    problem bishopDiagonal

---
+++ thuat_toan_co_ban/ma_tran/bishop_diagonal.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-02-09 23:54:26
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-02-09 23:58:37
+
+"""
+Trong thế giới cờ vua, con tượng không thích con tượng khác.
+Trong thực tế, khi 2 con tượng cùng đứng chung 1 đường chéo,
+chúng lập tức di chuyển tới hai đầu của đường chéo đó.
+
+Cho vị trí ban đầu của hai con tượng trên bàn cờ vua.
+Hãy tính toán vị trí của chúng sau khi di chuyển tới hai đầu của đường chéo
+mà chúng đang đứng.
+Nhớ rằng con tượng sẽ không di chuyển
+trừ khi chúng đứng chung trên một đường chéo.
+
+Ví dụ:
+- bishopDiagonal("d7", "f5") = ["c8", "h3"].
+- bishopDiagonal("d8", "b5") = ["b5", "d8"].
+"""
