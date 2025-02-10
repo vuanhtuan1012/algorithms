3ce16371d08e3d2733d860220d74194af1f7ca5e
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-10 23:02:24 +0700

    problem spiralNumbers

---
+++ thuat_toan_co_ban/ma_tran/spiral_numbers.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-02-10 23:00:35
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-02-10 23:02:00
+
+"""
+Cho số nguyên dương n.
+Hãy tạo ra ma trận vuông kích thước n*n chứa các số từ 1 tới n*n tăng dần
+theo hình xoắn ốc, xuất phát từ ô trên trái và đi theo theo theo chiều kim đồng hồ.
+
+Ví dụ:
+spiralNumbers(3) = [
+    [1, 2, 3],
+    [8, 9, 4],
+    [7, 6, 5]
+]
+"""
