f5cfca34961d3d54cc12e5b1dfc13366461fcbf4
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-11 23:58:06 +0700

    first step of problem spiralNumbers

--- thuat_toan_co_ban/ma_tran/spiral_numbers.py
+++ thuat_toan_co_ban/ma_tran/spiral_numbers.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-02-10 23:00:35
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-02-10 23:02:00
+# @Last Modified time: 2025-02-11 18:47:06
 
 """
 Cho số nguyên dương n.
@@ -15,4 +15,55 @@ spiralNumbers(3) = [
     [8, 9, 4],
     [7, 6, 5]
 ]
+spiralNumbers(4) = [
+    [ 1,  2,  3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+    [10,  9,  8, 7]
+]
 """
+from enum import Enum
+from typing import List
+
+class Direction(Enum):
+    LEFT_RIGHT = 0
+    TOP_DOWN = 1
+    RIGHT_LEFT = 2
+    BOTTOM_UP = 3
+
+
+def create_spiral_matrix(number: int) -> List[List[int]]:
+    matrix = [[0] * number] * number
+    left = 0
+    right = number - 1
+    val = 1
+    while val <= number**2:
+        for col in range(left, right + 1):
+            if val > number**2:
+                continue
+            matrix[left][col] = val
+            print(left, col, matrix[left][col])
+            val += 1
+        for row in range(left + 1, right + 1):
+            if val > number**2:
+                continue
+            matrix[row][right] = val
+            val += 1
+        for col in range(right - 1, left - 1, -1):
+            if val > number**2:
+                continue
+            matrix[right][col] = val
+            val += 1
+        for row in range(right - 1, left - 2, -1):
+            if val > number**2:
+                continue
+            matrix[row][left] = val
+            val += 1
+        left += 1
+        right -= 1
+    return matrix
+
+
+if __name__ == "__main__":
+    matrix = create_spiral_matrix(3)
+    print(matrix)
