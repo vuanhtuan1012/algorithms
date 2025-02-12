30b4a4ee24ae1f7f6160db54024f831d06fce220
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-02-12 15:45:33 +0700

    refactor the code of problem spiralNumbers

--- thuat_toan_co_ban/ma_tran/spiral_numbers.py
+++ thuat_toan_co_ban/ma_tran/spiral_numbers.py
@@ -2,7 +2,7 @@
 # @Author: VU Anh Tuan
 # @Date:   2025-02-10 23:00:35
 # @Last Modified by:   VU Anh Tuan
-# @Last Modified time: 2025-02-12 11:28:08
+# @Last Modified time: 2025-02-12 15:23:47
 
 """
 Cho số nguyên dương n.
@@ -24,42 +24,64 @@ spiralNumbers(4) = [
 """
 from typing import List
 
+Matrix = List[List[List]]
 
-def create_spiral_matrix(number: int) -> List[List[int]]:
+
+def fill_matrix_border(
+    lower_idx: int, upper_idx: int, matrix: Matrix, start_value: int
+) -> int:
+    """
+    Returns the value after fill in the matrix border
+    """
+    value = start_value
+    limit = len(matrix) ** 2
+
+    # left to right
+    for col in range(lower_idx, upper_idx + 1):
+        if value > limit:
+            return value
+        matrix[lower_idx][col] = value
+        value += 1
+
+    # top down
+    for row in range(lower_idx + 1, upper_idx + 1):
+        if value > limit:
+            return value
+        matrix[row][upper_idx] = value
+        value += 1
+
+    # right to left
+    for col in range(upper_idx - 1, lower_idx - 1, -1):
+        if value > limit:
+            return value
+        matrix[upper_idx][col] = value
+        value += 1
+
+    # bottom up
+    for row in range(upper_idx - 1, lower_idx, -1):
+        if value > limit:
+            return value
+        matrix[row][lower_idx] = value
+        value += 1
+    return value
+
+
+def create_spiral_matrix(number: int) -> Matrix:
     """
     Returns the spiral matrix of the given number
     """
+    # initialize matrix
     matrix = []
     for _ in range(number):
         matrix.append([0] * number)
-    left = 0
-    right = number - 1
-    val = 1
-    while val <= number**2:
-        # left to right
-        for col in range(left, right + 1):
-            if val > number**2:
-                return matrix
-            matrix[left][col] = val
-            val += 1
-        # top down
-        for row in range(left + 1, right + 1):
-            if val > number**2:
-                return matrix
-            matrix[row][right] = val
-            val += 1
-        # right to left
-        for col in range(right - 1, left - 1, -1):
-            if val > number**2:
-                return matrix
-            matrix[right][col] = val
-            val += 1
-        # bottom up
-        for row in range(right - 1, left, -1):
-            if val > number**2:
-                return matrix
-            matrix[row][left] = val
-            val += 1
-        left += 1
-        right -= 1
+
+    # fill in the matrix
+    lower_idx = 0  # lower index bound
+    upper_idx = number - 1  # upper index bound
+    value = 1
+    while value <= number**2:
+        # fill the matrix border from the outside in
+        value = fill_matrix_border(lower_idx, upper_idx, matrix, value)
+        lower_idx += 1
+        upper_idx -= 1
     return matrix
