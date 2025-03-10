7ba10a78c81993b31931f538834e6c7c3a037959
VU Anh Tuan <11141683+vuanhtuan1012@users.noreply.github.com>
2025-03-10 23:53:19 +0100

    solution bai 1

---
+++ high_school/de_vinh_phuc_2022_2023/bai_1.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# @Author: VU Anh Tuan
+# @Date:   2025-03-10 18:55:38
+# @Last Modified by:   VU Anh Tuan
+# @Last Modified time: 2025-03-10 19:50:51
+
+"""
+Bài 1. Trò chơi
+"""
+
+
+from typing import List, Tuple
+
+
+def is_prime(number: int) -> bool:
+    """
+    Returns True if number is prime, otherwise False
+    """
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def doc_file(filename: str) -> List[Tuple[int, int, int, int]]:
+    """
+    Returns list of numbers read from file
+    """
+    games = []
+    with open(filename, encoding="utf-8") as file_pointer:
+        for i, line in enumerate(file_pointer):
+            if i == 0:
+                continue
+            numbers = tuple(map(int, line.strip().split()))
+            games.append(numbers)
+    return games
+
+
+def find_winner(a: int, b: int, c: int, d: int) -> str:
+    """
+    Returns "Tam" if
+    """
+    for i in range(a, b + 1):
+        found_prime = False
+        for j in range(c, d + 1):
+            if is_prime(i + j):
+                found_prime = True
+                break
+        if not found_prime:
+            return "Tam"
+    return "Cam"
+
+
+if __name__ == "__main__":
+    games = doc_file("data/game.inp")
+    for numbers in games:
+        print(find_winner(*numbers))
